ecfddd19514728a63b083cc5c5c3a89ff2544b49
Francieudo <francieudo103@gmail.com>
2023-09-28 16:11:47 -0300

    4 commit
    alteração, remoção da pasta vagas e pasta sistema web

--- vagas/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Vaga  # Importe o modelo Vaga do seu arquivo models.py
-
-def lista_vagas(request):
-    vagas = Vaga.objects.all()  # Recupere todas as instâncias do modelo Vaga
-    context = {
-        'vagas': vagas  # Adicione as vagas ao contexto
-    }
-    return render(request, 'vagas/lista_vagas.html', lista_vagas())
