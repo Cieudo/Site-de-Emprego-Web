5b2ba081bd592d1adfab4e2a608c72e08d7c59e8
Quebrassonhos <55217021+Quebrassonhos@users.noreply.github.com>
2023-10-05 14:27:00 -0300

    Revert "tentativa de merge"

--- app_site_de_emprego/models.py
+++ app_site_de_emprego/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
-
 class Vaga(models.Model):
+    id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     empresa = models.CharField(max_length=100)
-    localidade = models.CharField(max_length=100)
 
     # Outros campos relevantes (localização, categoria, etc.)
 
     def __str__(self):
-        return self.titulo
+       return self.titulo 
+
+class Empresa(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+
+class Candidato(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+
+class Conta(models.Model):
+    id = models.AutoField(primary_key=True)
+
